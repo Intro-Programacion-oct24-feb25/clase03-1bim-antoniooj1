d6c2b88572e3d345bc2feee77929d0d9bf9e56b1
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-10-23 12:35:54 -0500

    cambios realizados

--- ejemplos-IDE/Ejemplo03/src/ejemplo03/Ejemplo03.java
+++ ejemplos-IDE/Ejemplo03/src/ejemplo03/Ejemplo03.java
@@ -37,7 +37,7 @@ public class Ejemplo03 {
         int variable2 = 10;
         int valorInicial = 18;
         int valor_inicial = 18;
-        // int valor inicial = 18;
+          // int valor inicial = 18;
         int valor_inicial_2 = 18;
 
         // Tipo de datos Double - Decimal
